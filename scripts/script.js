2d87511ab807f7d08dc88acb6644e03aa5db079e
Katie Duo Liu <katie_acting@live.ca>
2018-06-22 21:48:10 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,106 @@
+$(document).ready(() => {
+
+    $('#output').hide();
+    $('#loading').hide();
+
+    var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+
+    function doCORSRequest(options, printResult) {
+        var x = new XMLHttpRequest();
+        x.open(options.method, cors_api_url + options.url);
+        x.onload = x.onerror = function () {
+            printResult(
+                options.method + ' ' + options.url + '\n' +
+                x.status + ' ' + x.statusText + '\n\n' +
+                (x.responseText || '')
+            );
+        };
+        if (/^POST/i.test(options.method)) {
+            x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        }
+        x.send(options.data);
+    }
+    // Bind event
+    (function () {
+
+        var outputField = document.getElementById('output');
+
+
+            document.getElementById('submit-form').onclick = function (e) {
+
+                var url = 'https://piknu.com/u/' + $('#username').val() + '/following';
+
+                e.preventDefault();
+
+                for (i = 0; i < $('.col-sm-8').length; i++) {
+                    $('.col-sm-8')[i].style.display = 'none';
+                }
+
+                doCORSRequest({
+                    method: 'GET',
+                    url: url
+                }, function printResult(result) {
+
+                    var newResult = result.split("<");
+                    newResult.splice(0, 28);
+                    newResult.unshift(" ");
+                    newResult = newResult.join("<");
+
+
+                    $('#output').show();
+
+                    newResult = $.parseHTML(newResult);
+
+                    $('#output').append(newResult);
+
+                    $('#output').append("<button class='btn btn-custom' id='saveToDatabase'>Enter Into Database</button><button id='compare' class='btn btn-custom m-l' >Compare</button><a href='' class='btn btn-custom m-l'>Back</a>");
+
+                    var fArray = [];
+                    for ( i=0; i < $(".followx").length; i++ ) {
+                      var item = $('.followx').children("a:nth-child(2)")[i].innerHTML;
+                      fArray.push(item.toString());
+                    }
+
+
+                    $('#saveToDatabase').on('click', function() {
+
+                      var data = JSON.stringify({user: $('#username').val(), following: fArray});
+
+                      $.ajax({
+                        url: "http://localhost:5000/save",
+                        method: "POST",
+                        data: {user: $('#username').val(), following: fArray}
+                      }).then(res => {
+                        alert(res);
+                      })
+
+                    });
+
+
+                    $('#compare').on('click', function() {
+
+                      var data = JSON.stringify({user: $('#username').val(), following: fArray});
+
+                      function compareEm (){
+                        $.ajax({
+                          url: "http://localhost:5000/compare",
+                          method: "POST",
+                          data: {user: $('#username').val(), following: fArray},
+                          success: handleResponse
+                        })
+                      }
+                      function handleResponse(res) {
+                        $('#output').html("<h1 class='lead-h1'>Who They Recently Followed:<h1><br>" + res); 
+                      };
+
+                      compareEm();
+
+                    });
+
+
+
+                });
+            };
+    })();
+
+});
